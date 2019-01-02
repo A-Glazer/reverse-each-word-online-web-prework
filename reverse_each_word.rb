a27204d11bf7356926d9969d1acbedd5e0754a8b
A Glazer <avigayilglazer@gmail.com>
2019-01-02 20:58:47 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,12 +1,5 @@
 sentence = "Hello there, and how are you?"
 
-def reverse_each_word2(sentence)
-  reversed = sentence.reverse
-  reversed_in_array = reversed.split
-  flipped = reversed_in_array.reverse
-  flipped.join(" ")
-end
-
 def reverse_each_word(sentence)
   words = sentence.split
   backwards = []
@@ -14,4 +7,20 @@ def reverse_each_word(sentence)
      backwards << x.reverse
   end
   backwards.join(" ")
+end
+
+def reverse_each_word2(sentence)
+  words = sentence.split
+  backwards = []
+  words.each do |x|
+     backwards << x.reverse
+  end
+  backwards.join(" ")
+end
+
+def reverse_each_word3(sentence)
+  reversed = sentence.reverse
+  reversed_in_array = reversed.split
+  flipped = reversed_in_array.reverse
+  flipped.join(" ")
 end
