3f9f342d670dd19007dd37a11a1c460b237b5df4
A Glazer <avigayilglazer@gmail.com>
2019-01-02 19:40:33 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,13 +1,12 @@
 sentence = "Hello there, and how are you?"
 
-def reverse_without_each(sentence)
+def reverse_each_word(sentence)
   reversed = sentence.reverse
   reversed_in_array = reversed.split
   flipped = reversed_in_array.reverse
   flipped.join(" ")
 end
 
-def reverse_each_word(sentence)
-  sentence.split.each {|word| word.reverse}
-
+def reverse_collect_word(sentence)
+  sentence.map {|word| word.chars.my_reverse.join}.join(" ")
 end
