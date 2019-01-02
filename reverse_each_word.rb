39d20010c9da143de9afc009f9184f1a5cd75a15
A Glazer <avigayilglazer@gmail.com>
2019-01-02 20:37:06 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,13 +1,13 @@
 sentence = "Hello there, and how are you?"
 
-def reverse_each_word2(sentence)
+def reverse_each_word(sentence)
   reversed = sentence.reverse
   reversed_in_array = reversed.split
   flipped = reversed_in_array.reverse
   flipped.join(" ")
 end
 
-def reverse_each_word(sentence)
+def reverse_each_word2(sentence)
   words = sentence.split
   words.reverse_each {|word| puts word, " "}
 end
